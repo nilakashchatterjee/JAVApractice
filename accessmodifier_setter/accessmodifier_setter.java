9490d831fdc6f213202a00268321b858e08c5c73
nilakashchatterjee <work.nilakash@gmail.com>
2021-08-06 14:35:13 +0530

    first

--- accessmodifier_setter/accessmodifier_setter.java
+++ accessmodifier_setter/accessmodifier_setter.java
@@ -4,8 +4,8 @@ class Employee{
     private int id;
     private String name;
 
-    public void setname(String nn){
-        name = nn;
+    public void setname(String n){
+        name = n;
     }
     public String getname(){
         return name;

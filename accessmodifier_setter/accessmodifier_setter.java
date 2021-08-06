d5188311ddd9080ce50ddbf2e5a1780316d4e070
nilakashchatterjee <work.nilakash@gmail.com>
2021-08-06 13:37:37 +0530

    first

---
+++ accessmodifier_setter/accessmodifier_setter.java
@@ -0,0 +1,35 @@
+package accessmodifier_setter;
+
+class Employee{
+    private int id;
+    private String name;
+
+    public void setname(String n){
+        name = n;
+    }
+    public String getname(){
+        return name;
+    }
+    public void setid(int i){
+        id = i;
+    }
+    public int getid(){
+        return id;
+    }
+    //here we are using methods(getters,setters) to to access private data members 
+
+}
+
+public class accessmodifier_setter {
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        // emp.id=10;
+        // emp.name="Ajay"; --> error due to private access modifier
+        emp.setid(15);
+        System.out.println(emp.getid());
+        emp.setname("Nilakash Chatterjee");
+        System.out.println(emp.getname());
+
+    }
+    
+}

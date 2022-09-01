1a5a4a9797d66916d2924970d9006950294b2c7c
nilakashchatterjee <work.nilakash@gmail.com>
2022-09-02 01:52:04 +0530

    test

---
+++ employee/emp_1.java
@@ -0,0 +1,39 @@
+package employee;
+
+import java.util.Scanner;
+
+class test{
+    public test(String name, int empId) {
+        this.name = name;
+        this.empId = empId;
+    }
+    private String name;
+    private int empId;
+   
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getEmpId() {
+        return empId;
+    }
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+    
+
+
+}
+
+
+public class emp_1 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+
+    }
+
+}
